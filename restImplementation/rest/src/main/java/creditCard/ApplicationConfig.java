2e6cb50311e20538ce34691e7265235637dd12a9
TocaFonseca <49840274+TocaFonseca@users.noreply.github.com>
2021-12-12 09:50:27 +0000

    Delete restImplementation directory

--- restImplementation/rest/src/main/java/creditCard/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package creditCard;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/services")
-public class ApplicationConfig extends Application {
-}
